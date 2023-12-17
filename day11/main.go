3a92c90763cc35ee8c0720e33fe46395fa310706
noblekid96 <noblekid96@gmail.com>
2023-12-17 16:31:09 +0800

    Day 11

--- day11/main.go
+++ day11/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -24,7 +25,19 @@ var dirs = []Point{
 	Point{1,-0},
 }
 
+func Min(i,j int) int {
+	if i > j {
+		return j
+	}
+	return i
+}
 
+func Max(i,j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
 
 func main() {
 	input_file := os.Args[1]
@@ -46,7 +59,7 @@ func main() {
 		grid = append(grid, row)
 	}
 
-	fmt.Printf("Grid:\n%v\n", PrettyFormatGrid(grid))
+	// fmt.Printf("Grid:\n%v\n", PrettyFormatGrid(grid))
 
 	part1(grid)
 	part2(grid)
@@ -110,25 +123,7 @@ func part1(grid [][]string){
 		}
 	}
 
-	fmt.Printf("Empty rows:\n%v\n Empty cols:\n%v\n", PrettyFormat(emptyRows), PrettyFormat(emptyCols))
-
-	counter := 0
-	for _,r := range emptyRows{
-		grid = append(grid[:r+1], grid[r:]...)
-		grid[r+counter] = grid[r+counter+1]
-		counter++
-	}
-	fmt.Printf("Grid:\n%v\n", PrettyFormatGrid(grid))
-
-	counter = 0
-	for _,c := range emptyCols{
-		for i := range grid {
-			row := grid[i]
-			grid[i] = append(row[:c+counter], append([]string{"."}, row[c+counter:]...)...)
-		}
-		counter++
-	}
-	fmt.Printf("Grid:\n%v\n", PrettyFormatGrid(grid))
+	// fmt.Printf("Empty rows:\n%v\n Empty cols:\n%v\n", PrettyFormat(emptyRows), PrettyFormat(emptyCols))
 
 	m = len(grid)
 	n = len(grid[0])
@@ -142,59 +137,118 @@ func part1(grid [][]string){
 		}
 	}
 
-	fmt.Printf("Points:\n%v\n", PrettyFormat(points) )
+	// fmt.Printf("Points:\n%v\n. Len points: %v\n", PrettyFormat(points), len(points) )
 
-	path := map[Point]map[Point]int{}
+	scale := 2
+	total := 0
 
-	for len(points) > 0 {
-		sp := points[0]
+	for i, sp := range points{
+		for _, ep := range points[i+1:]{
+			x1 := sp.X
+			y1 := sp.Y
 
-		path[sp] = make(map[Point]int)
+			x2 := ep.X
+			y2 := ep.Y
 
+			for x := Min(x1,x2); x < Max(x1,x2); x ++ {
+				if slices.Contains(emptyRows, x){
+					total += scale
+				} else {
+					total += 1
+				}
+			}
 
-		q := []PointWithSteps{}
-		seen := map[Point]bool{}
-		q = append(q, PointWithSteps{sp, 0})
-		seen[sp] = true
-
-		for len(q) > 0 {
-			p := q[0]
-			q = q[1:]
-
-			for _, dir := range dirs {
-				dx := dir.X
-				dy := dir.Y
-
-				nx := p.P.X + dx
-				ny := p.P.Y + dy
-
-				if 0 <= nx && nx < len(grid) && 0 <= ny && ny < len(grid[0]){
-					if _, exists := seen[Point{nx,ny}]; !exists {
-						seen[Point{nx,ny}] = true
-						q = append(q, PointWithSteps{Point{nx,ny}, p.S+1})
-
-						if grid[nx][ny] == "#"{
-							path[sp][Point{nx,ny}] = p.S+1
-						}
-					}
+			for y := Min(y1,y2); y < Max(y1,y2); y ++ {
+				if slices.Contains(emptyCols, y){
+					total += scale
+				} else {
+					total += 1
 				}
 			}
 		}
-		points = points[1:]
 	}
 
-	fmt.Println(len(path))
-
-	for k,v := range path{
-		for kk, vv := range v{
-			fmt.Printf("Steps from %v to %v is : %v\n", k, kk, vv)
-		}
-	}
-
-
-	fmt.Printf("Paths: %v\n", PrettyFormat(path))
+	fmt.Printf("P1 Total steps: %v\n", total)
 }
 
 func part2(grid [][]string){
+	emptyCols := []int{}
+	emptyRows := []int{}
+	m := len(grid)
+	n := len(grid[0])
+
+	for i := 0; i < m; i ++ {
+		empty := true
+		for j := 0; j < n; j ++ {
+			if grid[i][j] == "#"{
+				empty = false
+				break
+			}
+		}
+		if empty {
+			emptyRows = append(emptyRows,i)
+			continue
+		}
+	}
+
+	for i := 0; i < n; i ++ {
+		empty := true
+		for j := 0; j < m; j ++ {
+			if grid[j][i] == "#"{
+				empty = false
+				break
+			}
+		}
+		if empty {
+			emptyCols = append(emptyCols,i)
+			continue
+		}
+	}
+	// fmt.Printf("Empty rows:\n%v\n Empty cols:\n%v\n", PrettyFormat(emptyRows), PrettyFormat(emptyCols))
+
+	m = len(grid)
+	n = len(grid[0])
+	points := []Point{}
+
+	for i := 0; i < m; i ++ {
+		for j := 0; j < n; j ++ {
+			if grid[i][j] == "#" {
+				points = append(points, Point{i,j})
+			}
+		}
+	}
+
+	// fmt.Printf("Points:\n%v\n. Len points: %v\n", PrettyFormat(points), len(points) )
+
+	scale := 1000000
+	total := 0
+
+	for i, sp := range points{
+		for _, ep := range points[i+1:]{
+			x1 := sp.X
+			y1 := sp.Y
+
+			x2 := ep.X
+			y2 := ep.Y
+
+			for x := Min(x1,x2); x < Max(x1,x2); x ++ {
+				if slices.Contains(emptyRows, x){
+					total += scale
+				} else {
+					total += 1
+				}
+			}
+
+			for y := Min(y1,y2); y < Max(y1,y2); y ++ {
+				if slices.Contains(emptyCols, y){
+					total += scale
+				} else {
+					total += 1
+				}
+			}
+		}
+	}
+
+	fmt.Printf("P2 Total steps: %v\n", total)
 
 }
